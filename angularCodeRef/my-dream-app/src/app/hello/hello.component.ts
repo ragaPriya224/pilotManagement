45345bac0f4e5968492f1e0ef41dd47698bc7938
bbb <aaa@gmail.com>
2022-06-22 01:12:03 +0530

    2.2 multi line html commit using ` symbol

--- angularCodeRef/my-dream-app/src/app/hello/hello.component.ts
+++ angularCodeRef/my-dream-app/src/app/hello/hello.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
-  template: '<div> Inline is working !! </div>',
+  template: `<div> Inline is working !!
+  will work it out</div>`,
   styleUrls: ['./hello.component.css']
 })
 export class HelloComponent implements OnInit {
